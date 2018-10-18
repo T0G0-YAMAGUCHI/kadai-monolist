ab7139d7bfce413beab144a2b0134cf170e0bcff
kumiko-sakai <ec2-user@ip-172-31-87-116.ec2.internal>
2018-10-18 13:32:17 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+  
   def new
     @items = []
     
@@ -15,6 +17,11 @@ class ItemsController < ApplicationController
         @items << item
       end
     end
+  end
+  
+  def show
+    @item=Item.find(params[:id])
+    @want_users = @item.want_users
   end
   
   private
